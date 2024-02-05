1ff8db2ab7c861d08b1fde3c85b6abf652618c9a
Abdullah708 <abdullahsunasara6@gmail.com>
2024-02-05 11:33:19 +0530

    Signals

--- blog/signals.py
+++ blog/signals.py
@@ -1,15 +1,34 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
 from django.contrib.auth.models import User
-
+from django.dispatch import receiver
 # Logic starts here
 
 
+@receiver(user_logged_in, sender=User)
 def login_success(sender, request, user, **kwargs):
     print("Logged-in Signal --------------->")
     print("Sender --------->", sender)
     print("Request --------->", request)
     print("User --------->", user)
-    print("kwargs ------>", kwargs)
+    print("User password ------->", user.password)
+    print(f"kwargs ------>{kwargs}")
 
 
-user_logged_in.connect(login_success, sender=User)
+@receiver(user_logged_out, sender=User)
+def log_out(sender, request, user, **kwargs):
+    print("Logout successful ------->")
+    print("Sender -------->", sender)
+    print("Request -------->", request)
+    print("User", user)
+    print(f"Kwargs -----> {kwargs}")
+
+
+@receiver(user_login_failed, sender=User)
+def login_failed(sender, request, user, **kwargs):
+    print("Login failed ------------>")
+    print("Sender ------------>", sender)
+    print("Request ------------>", request)
+    print("User --------->", user)
+    print(f"Kwargs {kwargs}")
+
+# user_logged_in.connect(login_success, sender=User)
